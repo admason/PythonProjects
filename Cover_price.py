c6ae7d4ffec8850568522a5b44fb5e5ea3d00d2a
Adam Mason <60157744+admason@users.noreply.github.com>
2020-08-17 21:28:29 +0100

    Cover_price.py
    Book Cost
    Suppose the cover price of a book is $24.95, but bookstores get a 40% discount.
    Shipping costs are $3 for the first copy and 75 cents for each additional copy.
    What is the total wholesale cost for 60 copies?

---
+++ Cover_price.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+#Volume of a sphere
+
+import math
+radius=float(input("Enter radius: "))
+def sph(radius):
+    return (4/3)*math.pi*radius**3
+print(sph(radius))
+
+
+# In[8]:
+
+
+# Wholesale costs
+
+coverprice = 24.95
+discount = 0.4
+quantity=int(input("Quantity? ")
+#ship = 3
+#shi=0.75
+             
+
+#if quantity=1:
+    ((quantity*coverprice)*0.6)-3
+#else:
+    (quantity*coverprice)*(1-discount)-(shipmany*quantity)
+
+
+# In[28]:
+
+
+#Book Cost
+
+
+
+cover=float(input("Cover cost?"))
+disc=0.4
+ship=3
+sship=0.75
+qty=int(input("Quantity? "))
+
+if qty==1:
+   cost=qty*cover*(1-disc)+(qty*ship) 
+else:
+    cost=(qty-1)*cover*(1-disc)+((qty-1)*sship)+ship 
+
+
+
+print(cost)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
